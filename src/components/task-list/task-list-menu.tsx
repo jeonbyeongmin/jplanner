c66f4e2044028c51f15b459e65f383a322bcf6fa
jeonbyeongmin <qudals7613@gmail.com>
2023-04-07 16:56:48 +0900

    feat: create task list in board

---
+++ src/components/task-list/task-list-menu.tsx
@@ -0,0 +1,24 @@
+import { IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { HiDotsVertical } from 'react-icons/hi'
+
+export function TaskListMenu() {
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label='Options'
+        icon={<HiDotsVertical />}
+        variant='ghost'
+        size='sm'
+        color='gray.500'
+        fontSize='lg'
+        visibility='hidden'
+        _groupHover={{ visibility: 'visible' }}
+      />
+      <MenuList>
+        <MenuItem>Edit title</MenuItem>
+        <MenuItem>Delete this list</MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
