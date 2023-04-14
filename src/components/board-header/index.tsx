6754b520336bd6f4b883a5966d2144470d928b22
jeonbyeongmin <qudals7613@gmail.com>
2023-04-14 21:22:59 +0900

    feat: creat board title machine

--- src/components/board-header/index.tsx
+++ src/components/board-header/index.tsx
@@ -1,41 +1,46 @@
 import { BoardHeaderMenu } from '@/components/board-header/board-header-menu'
-import { Button, Flex, IconButton, Input, Text, Tooltip } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { boardTitleMachine } from '@/machines/board-title-machine'
+import { Button, Flex, IconButton, Input, Text, Tooltip, useOutsideClick } from '@chakra-ui/react'
+import { useMachine } from '@xstate/react'
+import { useEffect, useRef } from 'react'
 import { HiPlus } from 'react-icons/hi'
 
-export function BoardHeader() {
-  const router = useRouter()
+interface BoardHeaderProps {
+  title: string
+}
 
-  // TODO: title, editmode 상태관리
-  const found = headerTitleData.find((item) => item.id === router.query.id)
+export function BoardHeader({ title }: BoardHeaderProps) {
+  const ref = useRef<HTMLDivElement>(null)
 
-  const [title, setTitle] = useState<string>('')
-  const [isEdit, setIsEdit] = useState<boolean>(false)
-
-  const handleModeChange = () => {
-    setTitle((prev) => prev.trim())
-    setIsEdit((prev) => !prev)
-  }
-
-  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-  }
+  const [current, send] = useMachine(boardTitleMachine, {
+    devTools: true,
+    context: {
+      title: title,
+      pendingTitle: title,
+    },
+  })
 
   const handleEnterKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      handleModeChange()
+      send('SUBMIT')
     }
   }
 
+  useOutsideClick({
+    ref: ref,
+    handler: current.matches('editing') ? () => send('CANCEL') : undefined,
+  })
+
   useEffect(() => {
-    if (found) {
-      setTitle(found.title)
-    }
-  }, [found])
+    send({
+      type: 'INITIATE',
+      value: title,
+    })
+  }, [send, title])
 
   return (
     <Flex
+      ref={ref}
       bgColor='white'
       h={20}
       borderBottom='1px'
@@ -47,35 +52,36 @@ export function BoardHeader() {
       w='full'
       position='absolute'
     >
-      {isEdit ? (
+      {current.matches('editing') ? (
         <Flex flex={1} gap={2}>
           <Input
             variant='outline'
-            defaultValue={title}
-            onBlur={handleModeChange}
-            onChange={handleTitle}
+            defaultValue={current.context.pendingTitle}
+            onChange={(e) => send('CHANGE', { value: e.target.value })}
             onKeyDown={handleEnterKeyDown}
             fontSize='xl'
             fontWeight='bold'
             p={2}
             autoFocus
           />
-          <Button>확인</Button>
-          <Button>취소</Button>
+          <Button onClick={() => send('SUBMIT')} disabled={!current.can({ type: 'SUBMIT' })}>
+            확인
+          </Button>
+          <Button onClick={() => send('CANCEL')}>취소</Button>
         </Flex>
       ) : (
         <Flex p={2} _hover={{ bgColor: 'gray.50' }} borderRadius='md'>
-          <Tooltip label={title} openDelay={1000}>
+          <Tooltip label={current.context.title} openDelay={1000}>
             <Text
               fontSize='xl'
               fontWeight='bold'
               cursor='pointer'
               userSelect='none'
-              onClick={handleModeChange}
+              onClick={() => send('EDIT')}
               noOfLines={1}
               color='blackAlpha.900'
             >
-              {title}
+              {current.context.title}
             </Text>
           </Tooltip>
         </Flex>
@@ -89,33 +95,3 @@ export function BoardHeader() {
     </Flex>
   )
 }
-
-const headerTitleData = [
-  { id: '1', title: '계획 제목을 입력하고 Enter를 누르면 저장됩니다. 취소하려면 다른 곳을 클릭해주세요' },
-  { id: '2', title: 'Plan Title2' },
-  { id: '3', title: 'Plan Title3' },
-  { id: '4', title: 'Plan Title4' },
-  { id: '5', title: 'Plan Title5' },
-  { id: '6', title: 'Plan Title6' },
-  { id: '7', title: 'Plan Title7' },
-  { id: '8', title: 'Plan Title8' },
-  { id: '9', title: 'Plan Title9' },
-  { id: '10', title: 'Plan Title10' },
-  { id: '11', title: 'Plan Title11' },
-  { id: '12', title: 'Plan Title12' },
-  { id: '13', title: 'Plan Title13' },
-  { id: '14', title: 'Plan Title14' },
-  { id: '15', title: 'Plan Title15' },
-  { id: '16', title: 'Plan Title16' },
-  { id: '17', title: 'Plan Title17' },
-  { id: '18', title: 'Plan Title18' },
-  { id: '19', title: 'Plan Title19' },
-  { id: '20', title: 'Plan Title20' },
-  { id: '21', title: 'Plan Title21' },
-  { id: '22', title: 'Plan Title22' },
-  { id: '23', title: 'Plan Title23' },
-  { id: '24', title: 'Plan Title24' },
-  { id: '25', title: 'Plan Title25' },
-  { id: '26', title: 'Plan Title26' },
-  { id: '27', title: 'Plan Title27' },
-]
