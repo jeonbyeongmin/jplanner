4f805651d9af8493d01184d10144c338d45bf5c0
jeonbyeongmin <qudals7613@gmail.com>
2023-04-17 21:50:39 +0900

    feat: add update board to machine

--- src/machines/board/board-machine.typegen.ts
+++ src/machines/board/board-machine.typegen.ts
@@ -1,26 +1,36 @@
-/* eslint-disable @typescript-eslint/ban-types */
 // This file was automatically generated. Edits will be overwritten
 
 export interface Typegen0 {
   '@@xstate/typegen': true
   'internalEvents': {
+    'done.invoke.board.adding:invocation[0]': {
+      type: 'done.invoke.board.adding:invocation[0]'
+      data: unknown
+      __tip: 'See the XState TS docs to learn how to strongly type this.'
+    }
     'xstate.init': { type: 'xstate.init' }
   }
   'invokeSrcNameMap': {
-    deleteBoard: 'done.invoke.boards.deleting:invocation[0]'
+    addBoard: 'done.invoke.board.adding:invocation[0]'
+    deleteBoard: 'done.invoke.board.deleting:invocation[0]'
+    updateBoard: 'done.invoke.board.updating:invocation[0]'
   }
   'missingImplementations': {
-    actions: never
+    actions: 'refetch'
     delays: never
     guards: never
     services: never
   }
-  'eventsCausingActions': {}
+  'eventsCausingActions': {
+    refetch: 'done.invoke.board.adding:invocation[0]'
+  }
   'eventsCausingDelays': {}
   'eventsCausingGuards': {}
   'eventsCausingServices': {
+    addBoard: 'ADD_BOARD'
     deleteBoard: 'DELETE_BOARD'
+    updateBoard: 'UPDATE_BOARD'
   }
-  'matchesStates': 'deleting' | 'failure' | 'idle' | 'waiting'
+  'matchesStates': 'adding' | 'deleting' | 'failure' | 'idle' | 'updating' | 'waiting'
   'tags': never
 }
