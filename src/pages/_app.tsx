06e18eabf3b66b7975d862acf0942548423fb35b
jeonbyeongmin <qudals7613@gmail.com>
2023-04-17 21:51:37 +0900

    feat: add log util

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,30 @@ import { Layout } from '@/components/layout'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import { GlobalStateProvider } from '@/contexts/global-state-provider'
+import { logOnBrowser } from '@/utils/logger'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-      }}
-    >
-      <ChakraProvider resetCSS>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </SWRConfig>
+    <GlobalStateProvider>
+      <SWRConfig
+        value={{
+          refreshInterval: 0,
+          revalidateIfStale: false,
+          revalidateOnFocus: false,
+          revalidateOnReconnect: false,
+          fetcher: (resource, init) => {
+            logOnBrowser(`fetching ${resource}`)
+            return fetch(resource, init).then((res) => res.json())
+          },
+        }}
+      >
+        <ChakraProvider resetCSS>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </SWRConfig>
+    </GlobalStateProvider>
   )
 }
---
+++ src/utils/logger.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+
+export const logOnBrowser = (error: unknown) => {
+  if (process.env.NODE_ENV === 'production') return
+  console.log(error)
+}
