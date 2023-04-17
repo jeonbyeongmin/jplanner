6266dc77489194678de0117eaa9985fdc36ab439
jeonbyeongmin <qudals7613@gmail.com>
2023-04-18 02:17:24 +0900

    build: install json-server

---
+++ server.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('data.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+    req.body.id = Math.random().toString(36).substr(2, 9)
+  }
+
+  next()
+})
+
+server.use(router)
+server.listen(4000, () => {
+  console.log('JSON Server is running')
+})
