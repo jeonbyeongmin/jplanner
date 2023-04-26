665d9dc0d24d5a03edbb98a3752ab326fc64fb74
jeonbyeongmin <qudals7613@gmail.com>
2023-04-26 14:21:28 +0900

    test: title이 비어있거나 인자가 undefined라면 에러를 던집니다

--- src/api/board/create-board.ts
+++ src/api/board/create-board.ts
@@ -7,6 +7,8 @@ export type CreateBoardParams = {
 export const createBoardPath = () => 'board';
 
 export const createBoardAPI = async (board: CreateBoardParams) => {
+  if (!board || !board.title) throw new Error('title is required');
+
   return await fetcher(createBoardPath(), {
     method: 'POST',
     data: board,
