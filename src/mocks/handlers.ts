e3a4fe49f4a569559272c3e7a8653dee50fc5b85
jeonbyeongmin <qudals7613@gmail.com>
2023-04-26 23:13:03 +0900

    refactor: update board 인자 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,12 @@
 import { rest } from 'msw';
 
-import { getBoardsPath } from '@/api/board/get-board';
+import { getBoardsPath } from '@/api/boards/paths';
 
 const board = [
+  // Retrieve boards
   rest.get(
     `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}`,
-    (req, res, ctx) => {
+    (_, res, ctx) => {
       return res(
         ctx.status(200),
         ctx.json([
@@ -24,15 +25,52 @@ const board = [
     },
   ),
 
-  rest.post(
-    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}`,
+  // Retrieve specific board
+  rest.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}/:id`,
     (req, res, ctx) => {
+      const { id } = req.params;
+
       return res(
         ctx.status(200),
         ctx.json({
-          message: 'success',
+          id,
+          name: `Board ${id}`,
+          description: `Description ${id}`,
         }),
       );
+    },
+  ),
+
+  // Create board
+  rest.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}`,
+    (_, res, ctx) => {
+      return res(ctx.status(201), ctx.json({}));
+    },
+  ),
+
+  // Update board
+  rest.put(
+    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}/:id`,
+    (_, res, ctx) => {
+      return res(ctx.status(200), ctx.json({}));
+    },
+  ),
+
+  // Partially update board
+  rest.patch(
+    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}/:id`,
+    (_, res, ctx) => {
+      return res(ctx.status(200), ctx.json({}));
+    },
+  ),
+
+  // Delete board
+  rest.delete(
+    `${process.env.NEXT_PUBLIC_API_URL}/${getBoardsPath()}/:id`,
+    (_, res, ctx) => {
+      return res(ctx.status(204), ctx.json({}));
     },
   ),
 ];
